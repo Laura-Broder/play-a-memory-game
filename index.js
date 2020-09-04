4becf21744cc04842d65adeb0351a60c0b498ae9
Laura Broder <laura.kokielov@gmail.com>
2020-09-04 23:53:21 +0300

    update

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const gameBoard = document.querySelector(".gameBoard");
+const playerName = document.querySelector(".playerName");
+const numOfErrors = document.querySelector(".numOfErrors");
+const bestPlayer = document.querySelector(".bestPlayer");
+const newGameBtn = document.querySelector(".startBtn");
+
+newGameBtn.addEventListener("click", startGame);
+
 let cardElementsArray = [];
 let cardObjArray = [];
-const simpleTheme = {
-  themeName: "simple",
-  themeImgArray: [1, 2, 3, 4, 5, 6],
+const animalTheme = {
+  themeName: "animal",
+  // themeImgArray: [
+  //   "url(./animal-theme/ashwini-chaudhary-dxZfz8VHA4Q-unsplash.jpg)",
+  //   "url(./animal-theme/birger-strahl-JqUUYJ8oUmo-unsplash.jpg)",
+  //   "url(./animal-theme/brittney-weng-YXq9lDyszBQ-unsplash.jpg)",
+  //   "url(./animal-theme/dusan-smetana-lwaaikBkiP8-unsplash.jpg)",
+  //   "url(./animal-theme/dustin-humes-qMuOXvGGIX8-unsplash.jpg)",
+  //   "url(./animal-theme/erda-estremera-CsLRfajosgA-unsplash.jpg)",
+  //   "url(./animal-theme/fabian-keller-2Bust437YkE-unsplash.jpg)",
+  //   "url(./animal-theme/julia-craice-o0S-0Pa4F2M-unsplash.jpg)",
+  //   "url(./animal-theme/karsten-winegeart-88lDyjdIDGw-unsplash.jpg)",
+  //   "url(./animal-theme/leon-bublitz-HetKVEbe9lM-unsplash.jpg)",
+  //   "url(./animal-theme/wolfgang-hasselmann-s-vNm9H3mRg-unsplash.jpg)",
+  //   "url(./animal-theme/66-north-PqDR1-8jv1c-unsplash.jpg)",
+  // ],
 };
 startGame();
 
 function startGame() {
+  cardElementsArray = [];
+  cardObjArray = [];
+  gameBoard.innerHTML = "";
   state();
   cardObjArray = createCardObjArray(state.numOfCards);
   cardElementsArray = createCardElementsArray(state.numOfCards);
+  playerName.innerText = `Player's name: ${state.playerName}`;
+  numOfErrors.innerText = `Number of Errors: ${state.numOfError}`;
+  bestPlayer.innerText = `Best Player: 
+  ${state.bestPlayer.bestPlayerName} with ${state.bestPlayer.bestPlayerNumOfErrors} Errors`;
 }
 
 function state() {
@@ -18,11 +45,14 @@ function state() {
   state.gameLevel = localStorage["chosenLevel"];
   state.numOfCards = getNumOfCards(state.gameLevel);
   state.cardsValuesArray = randomizeCardsArray(state.numOfCards);
-  state.theme = simpleTheme;
+  state.theme = animalTheme;
   state.gameStarted = true;
   state.numOfError = 0;
   state.flippedCards = [];
-  // state.bestPlayer= "";
+  state.bestPlayer = {
+    bestPlayerName: localStorage["bestPlayerName"],
+    bestPlayerNumOfErrors: localStorage["bestPlayerNumOfErrors"],
+  };
 }
 // get players name
 function getGameLevel() {
@@ -42,15 +72,22 @@ function getNumOfCards(gameLevel) {
   switch (gameLevel) {
     case "easy":
       numOfCards = 12;
+      gameBoard.classList.add("easy");
       break;
     case "medium":
       numOfCards = 18;
+      gameBoard.classList.add("medium");
+
       break;
     case "hard":
       numOfCards = 24;
+      gameBoard.classList.add("hard");
+
       break;
     default:
       numOfCards = 12;
+      gameBoard.classList.add("easy");
+
       break;
   }
   return numOfCards;
@@ -69,65 +106,98 @@ function randomizeCardsArray(arraySize) {
     if (cardsArray[i] > arraySize / 2)
       cardsArray[i] = cardsArray[i] - arraySize / 2;
   }
+  console.log(cardsArray);
   return cardsArray;
 }
 
 // functions on the card:
 
+function createCardObjArray(numOfCards) {
+  let cardObjArray = [];
+  for (let i = 0; i < numOfCards; i++) {
+    const card = {
+      cardValue: state.cardsValuesArray[i],
+      // cardImg: state.theme.themeImgArray[state.cardsValuesArray[i] - 1],
+      cardBack: state.theme.themeName,
+      flipped: false,
+      active: true,
+    };
+    cardObjArray.push(card);
+  }
+  console.log(cardObjArray);
+  return cardObjArray;
+}
+function updateCardObj(index, prop, newValue) {
+  cardObjArray[index][prop] = newValue;
+}
 function createCardElementsArray(numOfCards) {
   let cardElementsArray = [];
   for (let i = 0; i < numOfCards; i++) {
     const card = document.createElement("div");
+    card.innerText = cardObjArray[i].cardValue;
     card.classList.add("card");
     card.setAttribute("data-index", i);
-    card.innerHTML = state.cardsValuesArray[i];
+    card.setAttribute("data-value", cardObjArray[i].cardValue);
     card.setAttribute("data-flipped", false);
+    card.setAttribute("data-active", true);
     card.addEventListener("click", handleCardClick);
     gameBoard.appendChild(card);
     cardElementsArray.push(card);
   }
   return cardElementsArray;
 }
-function createCardObjArray(numOfCards) {
-  let cardObjArray = [];
-  for (let i = 0; i < numOfCards; i++) {
-    const card = {
-      cardValue: state.cardsValuesArray[i],
-      cardImg: state.theme.themeImgArray[state.cardsValuesArray[i] - 1],
-      cardBack: state.theme.themeName,
-      flipped: false,
-    };
-
-    cardObjArray.push(card);
-  }
-  return cardObjArray;
-}
 function handleCardClick(event) {
   const clickedCard = event.currentTarget;
-  console.log(state.flippedCards);
   flipCard(clickedCard);
+  // updateCardObj(index, prop, newValue);
+  clickedCard.setAttribute("data-active", false);
+  clickedCard.removeEventListener("click", handleCardClick);
   state.flippedCards.push(clickedCard);
   if (state.flippedCards.length === 2) {
-    if (!checkMatch(state.flippedCards)) {
-      cardElementsArray.forEach((cardElement) =>
-        cardElement.removeEventListener("click", handleCardClick),
-      );
-      setTimeout(wrongGuess, 1000);
-    }
+    const card1Index = Number(state.flippedCards[0].getAttribute("data-index"));
+    const card2Index = Number(state.flippedCards[1].getAttribute("data-index"));
+    checkMatch(card1Index, card2Index)
+      ? goodGuess(card1Index, card2Index)
+      : wrongGuess(card1Index, card2Index);
   }
 }
 function flipCard(clickedCard) {
   clickedCard.setAttribute("data-flipped", true);
 }
-function wrongGuess() {
-  activateCards();
+function goodGuess(card1Index, card2Index) {
   emptyFlippedArray();
+  if (document.querySelectorAll('[data-flipped="false"]').length === 0) {
+    winRestart();
+  }
 }
+function winRestart() {
+  localStorage["lastPlayerNumOfErrors"] = state.numOfError;
+  localStorage["lastPlayerName"] = state.playerName;
+  localStorage["continue"] = "yes";
+  if (!localStorage["bestPlayerName"]) {
+    localStorage["bestPlayerName"] = state.playerName;
+    localStorage["bestPlayerNumOfErrors"] = state.numOfError;
+  }
+  location.href = "./welcome.html";
+}
+function wrongGuess(card1Index, card2Index) {
+  state.numOfError++;
+  numOfErrors.innerHTML = `Number of Errors: ${state.numOfError}`;
+  cardElementsArray.forEach((cardElement) =>
+    cardElement.removeEventListener("click", handleCardClick),
+  );
+  activateWrongMatch(card1Index, card2Index);
+  setTimeout(activateCards, 1000);
+}
+
 function activateCards() {
   cardElementsArray.forEach((cardElement) => {
-    cardElement.addEventListener("click", handleCardClick);
-    flipBack(cardElement);
+    if (cardElement.getAttribute("data-active") === "true") {
+      cardElement.addEventListener("click", handleCardClick);
+      flipBack(cardElement);
+    }
   });
+  emptyFlippedArray();
 }
 function flipBack(cardElement) {
   cardElement.setAttribute("data-flipped", false);
@@ -135,19 +205,13 @@ function flipBack(cardElement) {
 function emptyFlippedArray() {
   state.flippedCards = [];
 }
-function checkMatch(flippedCards) {
-  const card1Index = Number(flippedCards[0].getAttribute("data-index"));
-  const card2Index = Number(flippedCards[1].getAttribute("data-index"));
-  if (
+function checkMatch(card1Index, card2Index) {
+  return (
     state.cardsValuesArray[card1Index] === state.cardsValuesArray[card2Index]
-  ) {
-    return true;
-  } else return false;
+  );
 }
-// display the stack
-// when clicked flip a card
-// allow 2 cards to flip
-// compare cards
-// if match leave flipped or hide from display
-// if no match leave a second for display and flip back
-// count if wrong guess
+
+function activateWrongMatch(card1Index, card2Index) {
+  cardElementsArray[card1Index].setAttribute("data-active", true);
+  cardElementsArray[card2Index].setAttribute("data-active", true);
+}
--- welcome.js
+++ welcome.js
@@ -1,4 +1,5 @@
 const startBtn = document.querySelector(".startBtn");
+const welcomeContainer = document.querySelector(".welcome-container");
 
 startBtn.addEventListener("click", function () {
   localStorage["chosenLevel"] = getGameLevel();
@@ -16,4 +17,23 @@ function getGameLevel() {
     }
   }
   return chosenLevel;
+}
+if (localStorage["continue"] === "yes") {
+  const lastPlayerNumOfErrors = localStorage["lastPlayerNumOfErrors"];
+  const lastPlayerName = localStorage["lastPlayerName"];
+
+  addMsgToPrevPlayer(lastPlayerName, lastPlayerNumOfErrors);
+}
+
+function addMsgToPrevPlayer(lastPlayerName, lastPlayerNumOfErrors) {
+  const playerName = document.createElement("h2");
+  const playerNumOfErrors = document.createElement("h2");
+  const playAgain = document.createElement("h2");
+
+  playerName.textContent = `GREAT JOB ${lastPlayerName}!!!`;
+  playerNumOfErrors.textContent = `You won the game with only ${lastPlayerNumOfErrors} errors!`;
+  playAgain.textContent = `Do you want to play again???`;
+  welcomeContainer.insertAdjacentElement("afterbegin", playAgain);
+  welcomeContainer.insertAdjacentElement("afterbegin", playerNumOfErrors);
+  welcomeContainer.insertAdjacentElement("afterbegin", playerName);
 }
